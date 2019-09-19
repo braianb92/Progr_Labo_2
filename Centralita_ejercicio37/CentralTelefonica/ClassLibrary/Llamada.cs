99027996011a72f344513a068519a74c0943efad
Baldino Braian <baldino.bab@gmail.com>
2019-09-18 23:19:18 -0300

    centralita

--- Centralita_ejercicio37/CentralTelefonica/ClassLibrary/Llamada.cs
+++ Centralita_ejercicio37/CentralTelefonica/ClassLibrary/Llamada.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+
     class Llamada
     {
         protected float duracion { get; }
-        protected string nroDestino;
-        protected string nroOrigen;
+        protected string nroDestino { get; }
+        protected string nroOrigen { get; }
+
+        public Enum TipoLlamada
+        {
+            Local,
+            Provincia,
+            Todas
+        }
+
+        public Llamada(float duracion,string nroDestino,string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+
+        public static int OrdenarPorDuracion(Llamada llamada1,Llamada llamada2)
+        {
+            if (llamada1.duracion > llamada2.duracion)
+                return 1;
+            else
+                return 0;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Duracion: {this.duracion}");
+            sb.AppendLine($"N° Origen: {this.nroDestino}");
+            sb.AppendLine($"N° Destino: {this.nroDestino}");
+            sb.AppendLine("---------------------");
+            return sb.ToString();
+        }
     }
 }
---
+++ Centralita_ejercicio37/CentralTelefonica/ClassLibrary/Local.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Local : Llamada
+    {
+        protected float costo;
+
+        public Local(Llamada llamada,float costo)
+        {
+            this.costo = costo;
+        }
+
+        public float CostoLlamada()
+        {
+            return this.costo;
+        }
+
+        private 
+
+
+    }
+}
