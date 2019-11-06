c6b713136dd46d23df5933d46bdfc9ed0560e47e
braianb92 <baldino.bab@gmail.com>
2019-11-05 22:12:53 -0300

    thread evento

---
+++ Guia 47+/Ejercicio 68/Ej_68/ClassLibrary/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //Delegate
+    public delegate void DelegadoString(string msg);
+    
+
+    public class Persona
+    {
+        //Atributes
+        private string nombre;
+        private string apellido;
+
+        //Event
+        public event DelegadoString EventoString;
+
+        //Properties
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return this.apellido; }
+            set {
+                this.EventoString("Cambio!");
+                apellido = value; }
+        }
+
+        //Constructor
+        public Persona()
+        {
+            nombre = string.Empty;
+            apellido = string.Empty;
+        }
+
+        //Methods
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Nombre} {Apellido}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Guia 47+/Ejercicio 68/Ej_68/FormsApp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApp
+{
+    public partial class FrmPersona : Form
+    {
+        private Persona persona;
+
+        public FrmPersona()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void FrmPersona_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            
+            if(!string.IsNullOrEmpty(txtNombre.Text) && !string.IsNullOrEmpty(txtApellido.Text))
+            {
+                Persona p = new Persona();
+                p.EventoString += NotificarCambio;
+
+                p.Nombre = txtNombre.Text;
+                p.Apellido = txtApellido.Text;
+                persona = p;
+                
+                
+                this.btnCrear.Text = "Actualizar";
+                
+            }
+        }
+
+        public void NotificarCambio(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+
+    }
+}
